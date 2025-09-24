b16f3ac3bc547f127d04b1b89371a512a0310cab
David Normington <davidnormo@users.noreply.github.com>
2025-09-24 16:23:53 +0100

    Update src.test.tsx

--- src.test.tsx
+++ src.test.tsx
@@ -4,7 +4,7 @@ import { createState } from "./src";
 
 React;
 
-const [useState, setState, getState] = createState(0);
+const [useState, setState, getState, subscribe] = createState(0);
 
 const TestComponent = () => {
   const state = useState((x) => x, []);
@@ -30,26 +30,37 @@ it("useState - get updated value", () => {
   const { result } = renderHook(() => useState((x) => x, []));
 
   act(() => {
-    setState(() => 1);
+    setState(1);
   });
 
   expect(result.current).toBe(1);
 });
 
+it("setState with function", () => {
+  setState(10);
+  const { result } = renderHook(() => useState((x) => x, []));
+
+  act(() => {
+    setState((x) => x + 1);
+  });
+
+  expect(result.current).toBe(11);
+});
+
 it("TestComponent - render with initial state", () => {
-  setState(() => 0);
+  setState(0);
   render(<TestComponent />);
   expect(screen.getByTestId("state").textContent).toBe("0");
 });
 
 it("getState", () => {
   expect(getState()).toBe(0);
-  setState(() => 1);
+  setState(1);
   expect(getState()).toBe(1);
 });
 
 it("encapsulation", () => {
-  setState(() => 1);
+  setState(1);
   const useDoubleState = () => {
     return useState((x) => x * 2, []);
   };
@@ -57,4 +68,195 @@ it("encapsulation", () => {
   const { result } = renderHook(() => useDoubleState());
 
   expect(result.current).toBe(2);
+});
+
+it("encapsulation - stringified", () => {
+  setState(1);
+  const useStrState = () => {
+    return useState((x) => `num: ${x}`, []);
+  };
+
+  const { result } = renderHook(() => useStrState());
+
+  expect(result.current).toBe("num: 1");
+});
+
+it("no zombie children allowed", () => {
+  setState(0);
+  const Parent = () => {
+    const value = useState((x) => x, []);
+
+    return (
+      <div>
+        <span data-testid="parent">{value}</span>
+        <TestComponent />
+      </div>
+    );
+  };
+
+  render(<Parent />);
+
+  expect(screen.getByTestId("parent").textContent).toBe("0");
+  expect(screen.getByTestId("state").textContent).toBe("0");
+
+  act(() => {
+    setState(1);
+  });
+
+  expect(screen.getByTestId("parent").textContent).toBe("1");
+  expect(screen.getByTestId("state").textContent).toBe("1");
+});
+
+it("doesn't care about async", async () => {
+  render(<TestComponent />);
+
+  async function delayed() {
+    await new Promise((r) => setTimeout(r, 100));
+    setState(100);
+  }
+
+  await act(() => delayed());
+
+  expect(screen.getByTestId("state").textContent).toBe("100");
+});
+
+it("subscribe", () => {
+  let i = 0;
+  const unsub = subscribe(() => {
+    i++;
+  });
+  setState(10);
+  setState(20);
+  setState(30);
+  expect(i).toBe(3);
+
+  unsub();
+  setState(40);
+  setState(50);
+  setState(60);
+  expect(i).toBe(3);
+});
+
+describe("complex object state", () => {
+  const [useObjState, setObjState] = createState({
+    a: { b: { c: 1, y: "bar" }, x: "foo" },
+  });
+
+  let renderCounts = { A: 0, B: 0, C: 0, App: 0 };
+
+  const RenderA = () => {
+    renderCounts.A++;
+    const state = useObjState((x) => JSON.stringify(x.a), []);
+    return <span data-testid="A">{state}</span>;
+  };
+  const RenderB = () => {
+    renderCounts.B++;
+    const state = useObjState((x) => JSON.stringify(x.a.b), []);
+    return <span data-testid="B">{state}</span>;
+  };
+  const RenderC = () => {
+    renderCounts.C++;
+    const state = useObjState((x) => x.a.b.c, []);
+    return <span data-testid="C">{state}</span>;
+  };
+
+  const App = () => {
+    renderCounts.App++;
+    return (
+      <>
+        <RenderA />
+        <RenderB />
+        <RenderC />
+      </>
+    );
+  };
+
+  beforeEach(() => {
+    renderCounts = { A: 0, B: 0, C: 0, App: 0 };
+  });
+
+  it("complex - update all", () => {
+    render(<App />);
+    expect(renderCounts).toEqual({ A: 1, B: 1, C: 1, App: 1 });
+    expect(screen.getByTestId("A").textContent).toBe(
+      '{"b":{"c":1,"y":"bar"},"x":"foo"}'
+    );
+    expect(screen.getByTestId("B").textContent).toBe('{"c":1,"y":"bar"}');
+    expect(screen.getByTestId("C").textContent).toBe("1");
+
+    act(() => {
+      setObjState((x) => {
+        return {
+          ...x,
+          a: {
+            ...x.a,
+            b: {
+              ...x.a.b,
+              c: 2,
+            },
+          },
+        };
+      });
+    });
+
+    expect(renderCounts).toEqual({ A: 2, B: 2, C: 2, App: 1 });
+    expect(screen.getByTestId("A").textContent).toBe(
+      '{"b":{"c":2,"y":"bar"},"x":"foo"}'
+    );
+    expect(screen.getByTestId("B").textContent).toBe('{"c":2,"y":"bar"}');
+    expect(screen.getByTestId("C").textContent).toBe("2");
+  });
+
+  it("complex - update A only", () => {
+    render(<App />);
+
+    expect(renderCounts).toEqual({ A: 1, B: 1, C: 1, App: 1 });
+    act(() => {
+      setObjState((x) => {
+        return {
+          ...x,
+          a: {
+            ...x.a,
+            x: "new foo",
+          },
+        };
+      });
+    });
+
+    expect(renderCounts).toEqual({ A: 2, B: 1, C: 1, App: 1 });
+
+    expect(screen.getByTestId("A").textContent).toBe(
+      '{"b":{"c":2,"y":"bar"},"x":"new foo"}'
+    );
+    expect(screen.getByTestId("B").textContent).toBe('{"c":2,"y":"bar"}');
+    expect(screen.getByTestId("C").textContent).toBe("2");
+  });
+
+  it("complex - update A and B only", () => {
+    render(<App />);
+
+    expect(renderCounts).toEqual({ A: 1, B: 1, C: 1, App: 1 });
+    act(() => {
+      setObjState((x) => {
+        return {
+          ...x,
+          a: {
+            ...x.a,
+            b: {
+              ...x.a.b,
+              y: "new bar",
+            },
+          },
+        };
+      });
+    });
+
+    expect(renderCounts).toEqual({ A: 2, B: 2, C: 1, App: 1 });
+
+    expect(screen.getByTestId("A").textContent).toBe(
+      '{"b":{"c":2,"y":"new bar"},"x":"new foo"}'
+    );
+    expect(screen.getByTestId("B").textContent).toBe('{"c":2,"y":"new bar"}');
+    expect(screen.getByTestId("C").textContent).toBe("2");
+  });
 });
